a57811b237787d5e00a02881d0531119236e15a8
Seungwoon <swdev678@gmail.com>
2021-10-10 21:08:40 +0900

    일시 커밋

--- shoppingmall/src/main/java/com/siblings/shoppingmall/domain/member/MemberServiceImpl.java
+++ shoppingmall/src/main/java/com/siblings/shoppingmall/domain/member/MemberServiceImpl.java
@@ -1,6 +1,5 @@
 package com.siblings.shoppingmall.domain.member;
 
-import com.siblings.shoppingmall.repository.MemberRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
